3e0028413506171208683d6766398b35afb576f3
Yaroslav Frehan <frehanyaroslav@gmail.com>
2024-05-10 17:33:08 +0200

    Add sqlite database

--- app.js
+++ app.js
@@ -1,65 +1,76 @@
 const express = require("express");
+const sqlite3 = require("sqlite3");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+const db = new sqlite3.Database(":memory:");
 
-let users = {};
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT)"
+  );
+});
 
 app.get("/users", (req, res) => {
-  res.status(200).json({ users });
+  db.all(`SELECT * FROM users`, [], (err, rows) => {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    res.json(rows);
+  });
 });
 
 app.get("/users/:id", (req, res) => {
   const { id } = req.params;
-  const user = users[id];
+  db.get(`SELECT * FROM users WHERE id = ?`, [id], (err, row) => {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    res.json(row); 
+  });
 
-  if (!user) {
-    res.status(404).json({ error: "Користувач не існує" });
-    return;
-  }
-
-  res.status(200).json({ user: users[id] });
 });
 
 app.post("/users", (req, res) => {
-  const { id, name, email } = req.body;
+  const { name, email } = req.body;
 
-  if (users[id]) {
-    res.status(409).json({ error: "Користувач вже існує" });
-    return;
-  }
-
-  users[id] = { name, email };
-  res.status(201).json({ user: users[id] });
+  db.run(
+    `INSERT INTO users (name, email) VALUES (?, ?)`,
+    [name, email],
+    function(err) {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      res.status(201).json({ id: this.lastID });
+    }
+  );
 });
 
 app.put("/users/:id", (req, res) => {
   const { id } = req.params;
   const { name, email } = req.body;
-  const user = users[id];
 
-  if (!user) {
-    res.status(404).json({ error: "User not found" });
-    return;
-  }
-
-  users[id] = { name, email };
-  res.status(200).json({ user: users[id]});
+  db.run(`UPDATE users SET name = ?, email = ? WHERE id = ?`, [name, email, id], function(err) {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    res.json({ message: `Row updated: ${this.changes}` })
+  });
+  
 });
 
 app.delete("/users/:id", (req, res) => {
   const { id } = req.params;
-  const user = users[id];
 
-  if (!user) {
-    res.status(404).json({ error: "User not found" });
-    return;
-  }
+  db.run(`DELETE FROM users WHERE id = ?`, [id], function(err) {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    res.json({ message: `Roe deleted: ${this.changes}` });
+  })
 
-  delete users[id];
-  res.status(200).json({ message: "User deleted successfullly" });
 });
 
 app.listen(port, () => {
