f6f247166c4d73187029f25823e66e703debb879
Yaroslav Frehan <frehanyaroslav@gmail.com>
2024-06-03 19:50:30 +0200

    Add login endpoint

--- app.js
+++ app.js
@@ -7,23 +7,21 @@ const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
 
-
-
 app.use(express.json());
 
 app.use((req, res, next) => {
   // console.log(req);
-  console.log("Метод:", req.method, "Шлях:", req.url, );
+  console.log("Метод:", req.method, "Шлях:", req.url);
   next();
 });
 
-const userSchema = Joi.object({ 
+const userSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
 });
 
-app.get('/status', (req, res) => {
-  res.status(200).send('Сервер працює');
+app.get("/status", (req, res) => {
+  res.status(200).send("Сервер працює");
 });
 
 app.get("/users", async (req, res) => {
@@ -34,7 +32,7 @@ app.get("/users", async (req, res) => {
   try {
     const users = await prisma.user.findMany();
     const usersSlice = users.slice(startIndex, endIndex);
-    res.json( usersSlice);
+    res.json(usersSlice);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -58,7 +56,6 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
-
 app.put("/users/:id", async (req, res) => {
   const { id } = req.params;
   const { name, email } = req.body;
@@ -77,46 +74,71 @@ app.put("/users/:id", async (req, res) => {
 });
 
 app.delete("/users/:id", async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  try {
-    await prisma.user.delete({
-      where: {
-        id: parseInt(id),
-      },
-    });
-    res.json({ message: "User deleted" });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+    try {
+        await prisma.user.delete({
+            where: {
+               id: parseInt(id),
+        },
+});
+        res.json({ message: "User deleted" });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 });
 
 app.post("/register", async (req, res) => {
-  const { name, password, email } = req.body;
+    const { name, password, email } = req.body;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = await prisma.user.create({
-      data: {
-        name,
-        hashedPassword,
-        email,
-      }
-    })
+        const user = await prisma.user.create({
+            data: {
+                name,
+                hashedPassword,
+                email,
+            }
+        })
+        
+        res.status(200).send("User was created");
+    } catch (err) {
+        res.status(500).send("Error while creating a user");
+    }       
+});
 
-    res.status(200).send("User was created");
-  } catch (err) {
-    res.status(500).send("Error while creating a user");
-  }
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+        if (!user) {
+            return res.status(401).send("No user found");
+        }
+
+        const isValid = await bcrypt.compare(password, user.hashedPassword);
+
+        if (!isValid) {
+            return res.status(401).send("Invalid password");
+        }
+
+        res.status(200).send("Login successful")
+    } catch (err) {
+        res.status(500).send("Login error");
+    }
 });
 
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-  })
+});
 }
-
 
 module.exports = app;
